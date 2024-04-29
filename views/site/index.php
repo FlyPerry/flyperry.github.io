9336e89487b00d7ced60dfdf9c114f207576df5f
Alex <01.alexsandr.46@gmail.com>
2024-04-29 22:30:59 +0500

    fix issues

--- views/site/index.php
+++ views/site/index.php
@@ -4,50 +4,301 @@
 
 $this->title = 'My Yii Application';
 ?>
-<div class="site-index">
+<div class="items_wrapper popular">
+    <h2>Популярные товары</h2>
+    <div class="items">
 
-    <div class="jumbotron text-center bg-transparent mt-5 mb-5">
-        <h1 class="display-4">Congratulations!</h1>
+        <div class="item" data-discount-type="percent" data-discount="0.00" data-code="61004">
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="https://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
-
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4 mb-3">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+            <a class="item_link" href="items/smartfon-apple-iphone-11-128gb-black/index.htm">
+                <div class="image image_size_400x400">
+                    <div class="item_tags">
+                        <div class="bestseller">Хит продаж</div>
+                    </div>
+                    <div class="bg_dark"></div>
+                    <img src="img/250x250/1018/items/1_1594629788.png"
+                         alt="Смартфон Apple iPhone 11 128GB Black" data-switch="0" width="250px"
+                         height="250px" loading="lazy">
+                    <img src="img/250x250/1018/items/1_1_1594629794.png"
+                         alt="Смартфон Apple iPhone 11 128GB Black" data-switch="1"
+                         style="display: none;" width="250px" height="250px">
+                </div>
+            </a>
+            <div class="item_content " data-discount="">
+                <div class="descr"><a href="items/smartfon-apple-iphone-11-128gb-black/index.htm">Смартфон
+                        Apple iPhone 11 128GB Black</a></div>
+                <div class="short_descr">В его компактном стильном корпусе поместился впечатляющий
+                    6,1-дюймовый экран Liquid Retina.
+                </div>
+                <div class="price ">
+                    <div class="new">
+                        64690.00
+                        руб.
+                    </div>
+                </div>
+                <div class="btn_wrapp">
+                    <a class="more" href="items/smartfon-apple-iphone-11-128gb-black/index.htm">Подробнее</a>
+                </div>
             </div>
-            <div class="col-lg-4 mb-3">
-                <h2>Heading</h2>
+        </div>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+        <div class="item" data-discount-type="percent" data-discount="0.00" data-code="61009">
 
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+            <a class="item_link" href="items/smartfon-samsung-galaxy-a51-64gb/index.htm">
+                <div class="image image_size_400x400">
+                    <div class="item_tags">
+                    </div>
+                    <div class="bg_dark"></div>
+                    <img src="img/250x250/1018/items/6_1594631642.png"
+                         alt="Смартфон Samsung Galaxy A51 64GB" data-switch="0" width="250px"
+                         height="250px" loading="lazy">
+                    <img src="img/250x250/1018/items/6_1_1594631647.png"
+                         alt="Смартфон Samsung Galaxy A51 64GB" data-switch="1"
+                         style="display: none;" width="250px" height="250px">
+                </div>
+            </a>
+            <div class="item_content " data-discount="">
+                <div class="descr"><a href="items/smartfon-samsung-galaxy-a51-64gb/index.htm">Смартфон
+                        Samsung Galaxy A51 64GB</a></div>
+                <div class="short_descr">Имеет два слота для SIM-карт, что позволяет пользоваться
+                    услугами разных мобильных операторов.
+                </div>
+                <div class="price ">
+                    <div class="new">
+                        21350.00
+                        руб.
+                    </div>
+                </div>
+                <div class="btn_wrapp">
+                    <a class="more" href="items/smartfon-samsung-galaxy-a51-64gb/index.htm">Подробнее</a>
+                </div>
             </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
+        </div>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+        <div class="item" data-discount-type="percent" data-discount="0.00" data-code="61013">
 
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+            <a class="item_link" href="items/smartfon-samsung-galaxy-s20-gray/index.htm">
+                <div class="image image_size_400x400">
+                    <div class="item_tags">
+                    </div>
+                    <div class="bg_dark"></div>
+                    <img src="img/250x250/1018/items/10_1594634478.png"
+                         alt="Смартфон Samsung Galaxy S20 Gray" data-switch="0" width="250px"
+                         height="250px" loading="lazy">
+                    <img src="img/250x250/1018/items/10_1_1594634481.png"
+                         alt="Смартфон Samsung Galaxy S20 Gray" data-switch="1"
+                         style="display: none;" width="250px" height="250px">
+                </div>
+            </a>
+            <div class="item_content " data-discount="">
+                <div class="descr"><a href="items/smartfon-samsung-galaxy-s20-gray/index.htm">Смартфон
+                        Samsung Galaxy S20 Gray</a></div>
+                <div class="short_descr">Имеет прочный корпус, который надежно защищает внутренние
+                    элементы от влаги и пыли.
+                </div>
+                <div class="price ">
+                    <div class="new">
+                        69950.00
+                        руб.
+                    </div>
+                </div>
+                <div class="btn_wrapp">
+                    <a class="more" href="items/smartfon-samsung-galaxy-s20-gray/index.htm">Подробнее</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="item" data-discount-type="percent" data-discount="0.00" data-code="61014">
+
+            <a class="item_link" href="items/smartfon-huawei-p30-breathing-crystal/index.htm">
+                <div class="image image_size_400x400">
+                    <div class="item_tags">
+                        <div class="recommend">Рекомендуем</div>
+                    </div>
+                    <div class="bg_dark"></div>
+                    <img src="img/250x250/1018/items/11_1594634924.png"
+                         alt="Смартфон Huawei P30 Breathing Crystal" data-switch="0" width="250px"
+                         height="250px" loading="lazy">
+                    <img src="img/250x250/1018/items/11_1_1594634927.png"
+                         alt="Смартфон Huawei P30 Breathing Crystal" data-switch="1"
+                         style="display: none;" width="250px" height="250px">
+                </div>
+            </a>
+            <div class="item_content " data-discount="">
+                <div class="descr"><a href="items/smartfon-huawei-p30-breathing-crystal/index.htm">Смартфон
+                        Huawei P30 Breathing Crystal</a></div>
+                <div class="short_descr">Тройная камера, состоящая из модулей на 40, 16 и 8 Мп
+                    позволяет снимать высококачественные фотографии и видео.
+                </div>
+                <div class="price ">
+                    <div class="new">
+                        31990.00
+                        руб.
+                    </div>
+                </div>
+                <div class="btn_wrapp">
+                    <a class="more" href="items/smartfon-huawei-p30-breathing-crystal/index.htm">Подробнее</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="item" data-discount-type="percent" data-discount="0.00" data-code="61015">
+
+            <a class="item_link" href="items/smartfon-huawei-nova-5t-midsummer/index.htm">
+                <div class="image image_size_400x400">
+                    <div class="item_tags">
+                        <div class="novelty">Новинка</div>
+                    </div>
+                    <div class="bg_dark"></div>
+                    <img src="img/250x250/1018/items/12_1594635327.png"
+                         alt="Смартфон Huawei Nova 5T Midsummer" data-switch="0" width="250px"
+                         height="250px" loading="lazy">
+                    <img src="img/250x250/1018/items/12_1_1594635329.png"
+                         alt="Смартфон Huawei Nova 5T Midsummer" data-switch="1"
+                         style="display: none;" width="250px" height="250px">
+                </div>
+            </a>
+            <div class="item_content " data-discount="">
+                <div class="descr"><a href="items/smartfon-huawei-nova-5t-midsummer/index.htm">Смартфон
+                        Huawei Nova 5T Midsummer</a></div>
+                <div class="short_descr">Яркие расцветки, удивительные текстуры и необычный дизайн
+                    смартфона HUAWEI nova 5T создают ощущение бесконечности пространства.
+                </div>
+                <div class="price ">
+                    <div class="new">
+                        24550.00
+                        руб.
+                    </div>
+                </div>
+                <div class="btn_wrapp">
+                    <a class="more" href="items/smartfon-huawei-nova-5t-midsummer/index.htm">Подробнее</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="item" data-discount-type="percent" data-discount="0.00" data-code="61016">
+
+            <a class="item_link" href="items/smartfon-huawei-p40-black/index.htm">
+                <div class="image image_size_400x400">
+                    <div class="item_tags">
+                    </div>
+                    <div class="bg_dark"></div>
+                    <img src="img/250x250/1018/items/13_1594635768.png"
+                         alt="Смартфон Huawei P40 Black (ANA-NX9)" data-switch="0" width="250px"
+                         height="250px" loading="lazy">
+                    <img src="img/250x250/1018/items/13_1_1594635770.png"
+                         alt="Смартфон Huawei P40 Black (ANA-NX9)" data-switch="1"
+                         style="display: none;" width="250px" height="250px">
+                </div>
+            </a>
+            <div class="item_content " data-discount="">
+                <div class="descr"><a href="items/smartfon-huawei-p40-black/index.htm">Смартфон
+                        Huawei P40 Black (ANA-NX9)</a></div>
+                <div class="short_descr">Ультракамера Ultra Vision Leica с тремя объективами и
+                    передовыми возможностями искусственного интеллекта.
+                </div>
+                <div class="price ">
+                    <div class="new">
+                        44690.00
+                        руб.
+                    </div>
+                </div>
+                <div class="btn_wrapp">
+                    <a class="more" href="items/smartfon-huawei-p40-black/index.htm">Подробнее</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="item" data-discount-type="percent" data-discount="0.00" data-code="61017">
+
+            <a class="item_link" href="items/smartfon-huawei-p40-lite-crush-green/index.htm">
+                <div class="image image_size_400x400">
+                    <div class="item_tags">
+                    </div>
+                    <div class="bg_dark"></div>
+                    <img src="img/250x250/1018/items/14_1594636188.png"
+                         alt="Смартфон Huawei P40 Lite Crush Green" data-switch="0" width="250px"
+                         height="250px" loading="lazy">
+                    <img src="img/250x250/1018/items/14_1_1594636191.png"
+                         alt="Смартфон Huawei P40 Lite Crush Green" data-switch="1"
+                         style="display: none;" width="250px" height="250px">
+                </div>
+            </a>
+            <div class="item_content " data-discount="">
+                <div class="descr"><a href="items/smartfon-huawei-p40-lite-crush-green/index.htm">Смартфон
+                        Huawei P40 Lite Crush Green</a></div>
+                <div class="short_descr">Смартфон Huawei P40 Lite Crush Green (JNY-LX1) оптимален
+                    как для общения и игр, так и для создания качественных снимков.
+                </div>
+                <div class="price ">
+                    <div class="new">
+                        19990.00
+                        руб.
+                    </div>
+                </div>
+                <div class="btn_wrapp">
+                    <a class="more" href="items/smartfon-huawei-p40-lite-crush-green/index.htm">Подробнее</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="item" data-discount-type="percent" data-discount="0.00" data-code="61018">
+
+            <a class="item_link" href="items/smartfon-huawei-p30-lite-peacock/index.htm">
+                <div class="image image_size_400x400">
+                    <div class="item_tags">
+                        <div class="bestseller">Хит продаж</div>
+                    </div>
+                    <div class="bg_dark"></div>
+                    <img src="img/250x250/1018/items/15_1594636400.png"
+                         alt="Смартфон Huawei P30 Lite Peacock" data-switch="0" width="250px"
+                         height="250px" loading="lazy">
+                    <img src="img/250x250/1018/items/15_1_1594636402.png"
+                         alt="Смартфон Huawei P30 Lite Peacock" data-switch="1"
+                         style="display: none;" width="250px" height="250px">
+                </div>
+            </a>
+            <div class="item_content " data-discount="">
+                <div class="descr"><a href="items/smartfon-huawei-p30-lite-peacock/index.htm">Смартфон
+                        Huawei P30 Lite Peacock</a></div>
+                <div class="short_descr">Смартфон Huawei MAR-LX1M Peacock Blue позволит вам
+                    насладиться увлекательным миром цифровых фотографий.
+                </div>
+                <div class="price ">
+                    <div class="new">
+                        13350.00
+                        руб.
+                    </div>
+                </div>
+                <div class="btn_wrapp">
+                    <a class="more" href="items/smartfon-huawei-p30-lite-peacock/index.htm">Подробнее</a>
+                </div>
             </div>
         </div>
 
     </div>
 </div>
+
+<div class="triggers">
+    <div class="trigger">
+        <i class="fas fa-map-marked-alt"></i>
+        <span><p>Доставляем заказы по всей территории России</p>
+</span>
+    </div>
+    <div class="trigger">
+        <i class="fas fa-shield-alt"></i>
+        <span><p>На всю продукцию предоставляется гарантия</p>
+</span>
+    </div>
+    <div class="trigger">
+        <i class="fas fa-money-check"></i>
+        <span><p>Покупатель может оплатить заказ любым удобным способом</p>
+</span>
+    </div>
+    <div class="trigger">
+        <i class="fas fa-truck-loading"></i>
+        <span><p>Широкий ассортимент телефонов от крупнейших поставщиков</p>
+</span>
+    </div>
+</div>
+
